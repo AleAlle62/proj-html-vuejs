baa84c8fcc58640618a8857fd1b50e5b2ca8bc90
AleAlle62 <basketallegrini@gmail.com>
2022-12-08 12:48:23 +0100

    secondo efetto slider

--- src/components/centralPage.vue
+++ src/components/centralPage.vue
@@ -14,7 +14,7 @@
 
                 <div>
 
-                    <header>
+                    <!-- <header>
                         <div class="row large">
                             <div class="imagegroup" style="border:2px solid white">
                             <img src="@/immagini/choco-chip-cookies-1200x1530.jpg" alt="">
@@ -23,15 +23,19 @@
                             <img src="@/immagini/cherry-cake-1200x1530.jpg" alt="">
                             </div>
                         </div>
-                    </header>
+                    </header> -->
 
                     <!-- ALTRA SOLUZIONE, statica uno alla volta --> 
 
-                    <!-- <div class="slider-container">
+                    <div class="slider-container">
                         <div class="slider">
                             <div class="slides">
                             <div id="slides__1" class="slide">
-                                <span class="slide__text"><img src="@/immagini/agency_thumbnail-400x300.jpg" alt=""></span>
+                                <span class="slide__text">
+                                    <img src="@/immagini/agency_thumbnail-400x300.jpg" alt="">
+                                    <img src="@/immagini/agency_thumbnail-400x300.jpg" alt="">
+                                </span>
+
                                 <a class="slide__prev" href="#slides__4" title="Next"></a>
                                 <a class="slide__next" href="#slides__2" title="Next"></a>
                             </div>
@@ -59,7 +63,7 @@
                             </div>
                         </div>
                     </div>
-                </div> -->
+                </div>
                 </div>
             </div>
 
@@ -212,7 +216,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    <!-- </div> -->
 </template>
 
 <script>
@@ -237,194 +241,194 @@ export default {
 <style lang="scss" scoped>
 
 
-// .slider-container {
-//   background: linear-gradient(
-//     149deg,
-//     rgb(247, 0, 255) 0%,
-//     rgb(255, 145, 0) 100%
-//   );
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   height: 100%;
-//   width: 500px;
-//   img {
-//     height: 100px;
-//   }
-// }
-
-// .slider {
-//   width: 100%;
-//   max-width: 600px;
-//   height: 400px;
-//   margin: 20px;
-//   text-align: center;
-//   border-radius: 20px;
-//   overflow: hidden;
-//   position: relative;
-// }
-
-// .slides {
-//   display: flex;
-//   overflow-x: scroll;
-//   position: relative;
-//   scroll-behavior: smooth;
-//   scroll-snap-type: x mandatory;
-// }
-
-// .slide:nth-of-type(even) {
-//   background-color: rgb(250, 246, 212);
-// }
-
-// .slide {
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   flex-shrink: 0;
-//   width: 100%;
-//   height: 400px;
-//   scroll-snap-align: center;
-//   margin-right: 0px;
-//   box-sizing: border-box;
-//   background: white;
-//   transform-origin: center center;
-//   transform: scale(1);
-// }
-
-// .slide__text {
-//   font-size: 40px;
-//   font-weight: bold;
-//   font-family: sans-serif;
-// }
-
-// .slide a {
-//   background: none;
-//   border: none;
-// }
-
-// a.slide__prev,
-// .slider::before {
-//   transform: rotate(135deg);
-//   -webkit-transform: rotate(135deg);
-//   left: 5%;
-// }
-
-// a.slide__next,
-// .slider::after {
-//   transform: rotate(-45deg);
-//   -webkit-transform: rotate(-45deg);
-//   right: 5%;
-// }
-
-// .slider::before,
-// .slider::after,
-// .slide__prev,
-// .slide__next {
-//   position: absolute;
-//   top: 48%;
-//   width: 35px;
-//   height: 35px;
-//   border: solid black;
-//   border-width: 0 4px 4px 0;
-//   padding: 3px;
-//   box-sizing: border-box;
-// }
-
-// .slider::before,
-// .slider::after {
-//   content: "";
-//   z-index: 1;
-//   background: none;
-//   pointer-events: none;
-// }
-
-// .slider__nav {
-//   box-sizing: border-box;
-//   position: absolute;
-//   bottom: 5%;
-//   left: 50%;
-//   width: 200px;
-//   margin-left: -100px;
-//   text-align: center;
-// }
-
-// .slider__navlink {
-//   display: inline-block;
-//   height: 15px;
-//   width: 15px;
-//   border-radius: 50%;
-//   background-color: black;
-//   margin: 0 10px 0 10px;
-// }
-
-
-
-
-
-
-
-
-// .read-article{
-//   position: absolute;
-//   top: 10px;
-//   left: 10px;
-//   z-index: 999;
-//   color: #000;
-//   background: white;
-//   padding: 10px 20px;
-//   border-radius: 10px;
-//   font-family: arial;
-//   text-decoration: none;
-//   box-shadow: rgb(50 50 93 / 25%) 0 0 100px -20px, rgb(0 0 0 / 30%) 0 0 60px -15px;
-// }
-// .read-article:hover{
-//     background: #d5d5d5;
-//     box-shadow: rgb(50 50 93 / 25%) 0 0 100px -20px, rgb(0 0 0 / 30%) 0 0 60px 0px;
-// }
-// iframe[sandbox] .read-article{
-//   display: none;
-// }
-
-
-
-
-
-header {
-    max-width: 1024px;
-    width: 100%;
-    margin-left: auto;
-    margin-right: auto;
-    overflow: hidden;
+.slider-container {
+  background: linear-gradient(
+    149deg,
+    rgb(247, 0, 255) 0%,
+    rgb(255, 145, 0) 100%
+  );
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 500px;
+  img {
+    height: 100px;
+  }
 }
 
-.row {
-    display: flex;
-    overflow: hidden;    
+.slider {
+  width: 100%;
+  max-width: 600px;
+  height: 400px;
+  margin: 20px;
+  text-align: center;
+  border-radius: 20px;
+  overflow: hidden;
+  position: relative;
 }
 
-.imagegroup {
-    display: flex;
-    height: 200px;
+.slides {
+  display: flex;
+  overflow-x: scroll;
+  position: relative;
+  scroll-behavior: smooth;
+  scroll-snap-type: x mandatory;
 }
 
-.imagegroup img{
-    flex-shrink: 0;
-    object-fit: cover;
-    border: 3px solid white;
+.slide:nth-of-type(even) {
+  background-color: rgb(250, 246, 212);
 }
 
-.imagegroup {
-    animation: travel 20s ease-in-out infinite;
+.slide {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  width: 100%;
+  height: 400px;
+  scroll-snap-align: center;
+  margin-right: 0px;
+  box-sizing: border-box;
+  background: white;
+  transform-origin: center center;
+  transform: scale(1);
 }
 
-@keyframes travel {
-    0%, 100% {
-        transform: translate3d(0,0,0);
-    }
-    50% {
-        transform: translate3d(-100%,0,0);
-    }
+.slide__text {
+  font-size: 40px;
+  font-weight: bold;
+  font-family: sans-serif;
 }
+
+.slide a {
+  background: none;
+  border: none;
+}
+
+a.slide__prev,
+.slider::before {
+  transform: rotate(135deg);
+  -webkit-transform: rotate(135deg);
+  left: 5%;
+}
+
+a.slide__next,
+.slider::after {
+  transform: rotate(-45deg);
+  -webkit-transform: rotate(-45deg);
+  right: 5%;
+}
+
+.slider::before,
+.slider::after,
+.slide__prev,
+.slide__next {
+  position: absolute;
+  top: 48%;
+  width: 35px;
+  height: 35px;
+  border: solid black;
+  border-width: 0 4px 4px 0;
+  padding: 3px;
+  box-sizing: border-box;
+}
+
+.slider::before,
+.slider::after {
+  content: "";
+  z-index: 1;
+  background: none;
+  pointer-events: none;
+}
+
+.slider__nav {
+  box-sizing: border-box;
+  position: absolute;
+  bottom: 5%;
+  left: 50%;
+  width: 200px;
+  margin-left: -100px;
+  text-align: center;
+}
+
+.slider__navlink {
+  display: inline-block;
+  height: 15px;
+  width: 15px;
+  border-radius: 50%;
+  background-color: black;
+  margin: 0 10px 0 10px;
+}
+
+
+
+
+
+
+
+
+.read-article{
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  z-index: 999;
+  color: #000;
+  background: white;
+  padding: 10px 20px;
+  border-radius: 10px;
+  font-family: arial;
+  text-decoration: none;
+  box-shadow: rgb(50 50 93 / 25%) 0 0 100px -20px, rgb(0 0 0 / 30%) 0 0 60px -15px;
+}
+.read-article:hover{
+    background: #d5d5d5;
+    box-shadow: rgb(50 50 93 / 25%) 0 0 100px -20px, rgb(0 0 0 / 30%) 0 0 60px 0px;
+}
+iframe[sandbox] .read-article{
+  display: none;
+}
+
+
+
+
+
+// header {
+//     max-width: 1024px;
+//     width: 100%;
+//     margin-left: auto;
+//     margin-right: auto;
+//     overflow: hidden;
+// }
+
+// .row {
+//     display: flex;
+//     overflow: hidden;    
+// }
+
+// .imagegroup {
+//     display: flex;
+//     height: 200px;
+// }
+
+// .imagegroup img{
+//     flex-shrink: 0;
+//     object-fit: cover;
+//     border: 3px solid white;
+// }
+
+// .imagegroup {
+//     animation: travel 20s ease-in-out infinite;
+// }
+
+// @keyframes travel {
+//     0%, 100% {
+//         transform: translate3d(0,0,0);
+//     }
+//     50% {
+//         transform: translate3d(-100%,0,0);
+//     }
+// }
 
 
 
